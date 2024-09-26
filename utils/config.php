2781bc26d5ad74f88fb304a404bb5d12b488c393
DANANG Adi Nur Saputro <danangadi949@gmail.com>
2024-09-26 16:45:42 +0700

    first commit

---
+++ utils/config.php
@@ -0,0 +1,18 @@
+<?php
+
+function config()
+{
+    return [
+        'app'   => [
+            'url'   => 'http://localhost:9000',
+            'name'  => 'Galery APP'
+        ],
+        'database'  => [
+            'port'  => 3306,
+            'host'  => 'localhost',
+            'username'  => 'root',
+            'password'  => '',
+            'name'      => 'galeries'
+        ]
+    ];
+}
